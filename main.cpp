fb39bb295c6b5870f17a3cc29fd4cdae889ac03d
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2021-12-07 12:24:45 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 Daniel Pawelko All rights reserved.
+//
+// Created by: Daniel Pawelko
+// Created on: Dec 2021
+// For Loops
+
+#include <iostream>
+
+int main() {
+    // main function for creating for loop program
+    // define variables
+    int number;
+
+    // input
+    std::cout << "Please enter a number: " << std::endl;
+    std::cin >> number;
+
+    // process/output
+	for (int i = 0; i < number + 1; i++) {
+		std::cout << i << "² = " << i*i << std::endl;
+	}
+
+    // output finished
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
